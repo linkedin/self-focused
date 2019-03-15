8c17a4c096e049a6126a219bb5985cb3e76bf621
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2019-03-15 08:54:17 -0700

    fix(fastboot): verify not fastboot prior accessing this.element

--- packages/ember-self-focused/addon/components/self-focused.js
+++ packages/ember-self-focused/addon/components/self-focused.js
@@ -1,11 +1,17 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import { getOwner } from '@ember/application';
 import layout from '../templates/components/self-focused';
 
 export default Component.extend({
   layout,
   focusManager: service(),
   classNames: ['self-focused'],
+
+  fastboot: computed(function() {
+    return getOwner(this).lookup('service:fastboot');
+  }),
 
   init() {
     this._super(...arguments);
@@ -29,7 +35,7 @@ export default Component.extend({
      * and for the very first invocation, the corresponding element is not available
      * as it is yet to be inserted in the DOM
      */
-    if (this.element) {
+    if (!this.get('fastboot.isFastBoot') && this.element) {
       this.get('focusManager').setNodeToBeFocused(this.element, 'attr');
     }
   }
